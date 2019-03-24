18224ea8a4a4398e7494f1cc4b3e6939d601408d
xsnowholy <495904127@qq.com>
2019-03-24 20:22:44 +0800

    更新线上请求地址

--- src/axios/baseURL.js
+++ src/axios/baseURL.js
@@ -1,4 +1,4 @@
-const DEFAULT_URL = 'https://pes2.kuxiao.cn';
+const DEFAULT_URL = 'https://easy-mock.com/mock/5c97752f38f3e126d53c8c2a';
 const G = window.g;
 const BASE_URL = G ? G.baseUrl: DEFAULT_URL;
 
